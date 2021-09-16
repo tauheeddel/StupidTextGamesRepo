94a00d62fcfcdc56fd03e864445d2a83c532c0cd
Admin <Admin@DESKTOP-9UM1MPL.lan>
2021-09-16 18:58:13 -0500

    Adding HeavenOrHell

---
+++ StupidTextGamesProject/src/HeavenOrHell.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class HeavenOrHell implements StupidTextGame {
+	
+	public String getName() {
+		return "HeavenOrHell";
+	}
+	
+	public void play(Scanner console) {
+		String[] pick = {"Heaven", "Hell"};
+		Random rand = new Random();
+		
+		int index = rand.nextInt(pick.length);
+		String choice = pick[index];
+		
+		if (choice.equals("Hell")) {
+			System.out.println("Its Hell!!! You die");
+		}else {
+			System.out.println("ITS Heaven!!! You live");
+		}
+	}
+
+}
