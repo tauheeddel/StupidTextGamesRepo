8502d05cfda8e8616cc6154caf9e616f9e172e97
justincassens18 <justincassens18@OlinLab204-03.augustana.edu>
2021-09-16 12:11:47 -0500

    One stupid chicken

---
+++ StupidTextGamesProject/src/ChickenCrossRoad.java
@@ -0,0 +1,17 @@
+//Justin Cassens
+import java.util.Scanner;
+
+public class ChickenCrossRoad implements StupidTextGame {
+
+	@Override
+	public String getName() {
+		return "Chicken Road Crossing";
+	}
+
+	@Override
+	public void play(Scanner console) {
+		System.out.print("Your chicken's agility was lacking. It was flattened!");
+		
+	}
+
+}
