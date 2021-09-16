1f2a0703fce6fcbdebdc62bc6678959ddee80d17
tannerklein20 <tannerklein20@OlinLab204-19.augustana.edu>
2021-09-16 12:13:09 -0500

    new comment

--- StupidTextGamesProject/src/StupidTextGameLauncher.java
+++ StupidTextGamesProject/src/StupidTextGameLauncher.java
@@ -12,6 +12,7 @@ public class StupidTextGameLauncher {
 		games.add(new NumberGuesser());
 		games.add(new DungeonEscape());
 		games.add(new StupidZombieGame());
+		// Hey
 		
 		System.out.println("Choose a stupid text game:");
 		for (int i = 0; i < games.size(); i++) {
