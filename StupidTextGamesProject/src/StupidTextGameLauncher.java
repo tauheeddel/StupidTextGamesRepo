c6fbdc23784a9cf39cb4181ca2135194c2b8c511
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-09-17 21:54:36 -0500

    Adding Motivational Quotes File

--- StupidTextGamesProject/src/StupidTextGameLauncher.java
+++ StupidTextGamesProject/src/StupidTextGameLauncher.java
@@ -12,6 +12,7 @@ public class StupidTextGameLauncher {
 		games.add(new NumberGuesser());
 		games.add(new DungeonEscape());
 		games.add(new GetGoodDayQuotesGame());
+		games.add(new MotivationalQuotes());
 		
 		System.out.println("Choose a stupid text game:");
 		for (int i = 0; i < games.size(); i++) {
